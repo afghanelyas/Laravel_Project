97468be7c6d34fd30882caea0131713f8fd6c99d
afghanelyas <afghan.elyas1@gmail.com>
2023-05-14 09:37:50 +0430

    Store blog posts as html files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,20 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css" />
     <title>Document</title>
 </head>
 <body>
 <article>
-    <h1><a href="/post">My First Post</a></h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
-    </p>
-
+   <?= $post; ?>
 </article>
 <a href="/">Go back</a>
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,16 +9,39 @@
 </head>
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+    </p>
     </article>
     <article>
         <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>    
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+        orem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        </p>    
     </article>
     <article>
         <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.
+            orem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam
+        </p>
     </article>
 </body>
 </html>
