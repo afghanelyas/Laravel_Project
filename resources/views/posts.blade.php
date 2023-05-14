4e6073ec7c472c7cb5076eb3161895f67b05f950
afghanelyas <afghan.elyas1@gmail.com>
2023-05-14 06:43:01 +0430

    Route Multiple pages

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css" />
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>    
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quibusdam.</p>
+    </article>
+</body>
+</html>
